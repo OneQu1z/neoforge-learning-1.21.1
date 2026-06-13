ad317ece64a472090186aa79990c866b0ed7c7b0
Dmitry Kochelyagin <taynov.pro@outlook.com>
2026-06-13 14:58:25 +0300

    Add: Добавил стамеску, которая умеет превращать блоки в другие блоки

--- src/main/java/ru/onequ1z/tutorialmod/item/ModItems.java
+++ src/main/java/ru/onequ1z/tutorialmod/item/ModItems.java
@@ -6,6 +6,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import ru.onequ1z.tutorialmod.TutorialMod;
+import ru.onequ1z.tutorialmod.item.custom.ChiselItem;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MOD_ID);
@@ -15,6 +16,9 @@ public class ModItems {
 
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
                 () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+                    () -> new ChiselItem(new Item.Properties().durability(32)));
 
 
 
